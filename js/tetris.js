68424e97018aa67f4eea16d56dab9d9be21fa6d1
Mustafa Ucuncu <mustafaucuncu@onet.pl>
2018-10-24 01:22:40 +0200

    Create proto of game

--- js/tetris.js
+++ js/tetris.js
@@ -7,6 +7,72 @@ const SQ = 20; //Square size
 
 const VACANT = 'white';
 
+const PIECES = [
+    [Z, "red"],
+    [S, "green"],
+    [T, "yellow"],
+    [O, "blue"],
+    [L, "purple"],
+    [I, "cyan"],
+    [J, "orange"]
+];
+
+class Piece {
+    constructor(tetromino, color) {
+        this.tetromino = tetromino;
+        this.color = color;
+        this.tetrominoNumber = 0;
+        this.activeTetromino = this.tetromino[this.tetrominoNumber];
+
+        this.x = 3;
+        this.y = -2;
+
+        this.timerID = setInterval(this.moveDown.bind(this), 1000);
+    }
+
+    draw() {
+        for(let r = 0; r < this.activeTetromino.length; r++) {
+            for(let c = 0; c < this.activeTetromino.length; c++) {
+                if(this.activeTetromino[r][c]) {
+                    drawSquare(this.x + c, this.y + r, this.color);
+                }
+            }
+        }
+    }
+
+    undraw() {
+        for(let r = 0; r < this.activeTetromino.length; r++) {
+            for(let c = 0; c < this.activeTetromino.length; c++) {
+                drawSquare(this.x + c, this.y + r, VACANT);
+            }
+        }
+    }
+
+    moveRight() {
+        this.undraw();
+        this.x++;
+        this.draw();
+    }
+
+    moveLeft() {
+        this.undraw();
+        this.x--;
+        this.draw();
+    }
+
+    moveDown() {
+        this.undraw();
+        this.y++;
+        this.draw();
+    }
+    
+    rotate() {
+        this.undraw();
+        this.tetrominoNumber = (this.tetrominoNumber + 1) % 4;
+        this.activeTetromino = this.tetromino[this.tetrominoNumber];
+        this.draw();
+    }
+}
 
 //SQUARE drawing
 
@@ -23,7 +89,7 @@ function drawSquare(x, y, color){
 
 let board = [];
 
-for(let r = 0; r < ROW; r++) {
+for(let r = 0; r < ROW; r++) { //init board as empty
     board[r] = [];
 
     for(let c = 0; c < COL; c++) {
@@ -40,3 +106,40 @@ function drawBoard() {
 }
 
 drawBoard();
+
+
+//Get random piece
+
+function randomPiece() {
+    let r = Math.floor(Math.random() * PIECES.length);
+    return PIECES[r][0];
+}
+
+function randomColor() {
+    let r = Math.floor(Math.random() * PIECES.length);
+    return PIECES[r][1];
+}
+
+//initiate a new piece
+
+function createPiece() {
+    let p = new Piece(randomPiece(), randomColor());
+    window.addEventListener('keypress', (event) => { //arrow controls(move, rotate)
+        if(event.keyCode == 37) {
+            p.moveLeft();
+        }
+        if(event.keyCode == 38) {
+            p.rotate();
+        }
+        if(event.keyCode == 39) {
+            p.moveRight();
+        }
+        if(event.keyCode == 40) {
+            p.moveDown();
+        }
+        
+    });
+
+    return p;
+}
+
